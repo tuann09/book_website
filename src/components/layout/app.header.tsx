d452c9e525b7072f4c582558e17d5b2bb968767b
Tuans09 <dt2003911@gmail.com>
2025-06-13 17:10:15 +0700

    preview cart

--- src/components/layout/app.header.tsx
+++ src/components/layout/app.header.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FaReact } from "react-icons/fa";
 import { FiShoppingCart } from "react-icons/fi";
 import { VscSearchFuzzy } from "react-icons/vsc";
-import { Divider, Badge, Drawer, Avatar, Popover } from "antd";
+import { Divider, Badge, Drawer, Avatar, Popover, Empty } from "antd";
 import { Dropdown, Space } from "antd";
 import { useNavigate } from "react-router";
 import "./app.header.scss";
@@ -69,28 +69,35 @@ const AppHeader = (props: any) => {
     const contentPopover = () => {
         return (
             <div className="pop-cart-body">
-                {/* <div className='pop-cart-content'>
+                <div className="pop-cart-content">
                     {carts?.map((book, index) => {
                         return (
-                            <div className='book' key={`book-${index}`}>
-                                <img src={`${import.meta.env.VITE_BACKEND_URL}/images/book/${book?.detail?.thumbnail}`} />
+                            <div className="book" key={`book-${index}`}>
+                                <img
+                                    src={`${
+                                        import.meta.env.VITE_BACKEND_URL
+                                    }/images/book/${book?.detail?.thumbnail}`}
+                                />
                                 <div>{book?.detail?.mainText}</div>
-                                <div className='price'>
-                                    {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(book?.detail?.price ?? 0)}
+                                <div className="price">
+                                    {new Intl.NumberFormat("vi-VN", {
+                                        style: "currency",
+                                        currency: "VND",
+                                    }).format(book?.detail?.price ?? 0)}
                                 </div>
                             </div>
-                        )
+                        );
                     })}
                 </div>
-                {carts.length > 0 ?
-                    <div className='pop-cart-footer'>
-                        <button onClick={() => navigate('/order')}>Xem giỏ hàng</button>
+                {carts.length > 0 ? (
+                    <div className="pop-cart-footer">
+                        <button onClick={() => navigate("/order")}>
+                            Xem giỏ hàng
+                        </button>
                     </div>
-                    :
-                    <Empty
-                        description="Không có sản phẩm trong giỏ hàng"
-                    />
-                } */}
+                ) : (
+                    <Empty description="Không có sản phẩm trong giỏ hàng" />
+                )}
             </div>
         );
     };
