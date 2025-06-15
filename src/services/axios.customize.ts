811cb1886b9b5f7443db850820a360b656034b0d
Tuans09 <dt2003911@gmail.com>
2025-06-15 10:19:38 +0700

    access_token va refresh theo tuan tu

--- src/services/axios.customize.ts
+++ src/services/axios.customize.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { Mutex } from "async-mutex";
+const mutex = new Mutex();
 
 const createInstanceAxios = (baseURL: string) => {
     const instance = axios.create({
@@ -6,6 +8,13 @@ const createInstanceAxios = (baseURL: string) => {
         baseURL: baseURL,
         withCredentials: true,
     });
+    const handleRefreshToken = async () => {
+        return await mutex.runExclusive(async () => {
+            const res = await instance.get("/api/v1/auth/refresh");
+            if (res && res.data) return res.data.access_token;
+            else return null;
+        });
+    };
 
     // Add a request interceptor
     instance.interceptors.request.use(
@@ -32,9 +41,23 @@ const createInstanceAxios = (baseURL: string) => {
             }
             return response;
         },
-        function (error) {
+        async function (error) {
             // Any status codes that falls outside the range of 2xx cause this function to trigger
             // Do something with response error
+            if (
+                error.config &&
+                error.response &&
+                +error.response.status === 401
+            ) {
+                const access_token = await handleRefreshToken();
+                if (access_token) {
+                    error.config.headers[
+                        "Authorization"
+                    ] = `Bearer ${access_token}`;
+                    localStorage.setItem("access_token", access_token);
+                    return instance.request(error.config);
+                }
+            }
             if (error && error.response && error.response.data) {
                 return error.response.data;
             }
